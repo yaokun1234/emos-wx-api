67e2af4e2779323e631abddb919742514735b4c3
simo <2921847833@qq.com>
2022-12-28 18:53:25 +0800

    初始化项目

---
+++ src/main/java/com/simo/emos/wx/EmosWxApiApplication.java
@@ -0,0 +1,13 @@
+package com.simo.emos.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosWxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosWxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simo/emos/wx/EmosWxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simo.emos.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmosWxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
