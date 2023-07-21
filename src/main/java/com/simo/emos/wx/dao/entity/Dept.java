f49aff5798b145e22f237c09d7a8bdef9ecdcb45
simo <2921847833@qq.com>
2023-07-21 11:18:05 +0800

    perf(*):更新3

--- src/main/java/com/simo/emos/wx/dao/entity/Dept.java
+++ src/main/java/com/simo/emos/wx/dao/entity/Dept.java
@@ -17,7 +17,7 @@ public class Dept {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
-    @ApiModelProperty("部门名称2")
+    @ApiModelProperty("部门名称3")
     @Column(name = "dept_name", nullable = false)
     private String deptName;
 
